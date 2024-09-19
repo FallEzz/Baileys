fed0cc3716d8cab891381baadf0547ef60ec9d8a
FallEzz <116632411+FallEzz@users.noreply.github.com>
2024-09-19 14:11:53 +0700

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { proto } from '../WAProto'
+import makeWASocket from './Socket'
+
+export * from '../WAProto'
+export * from './Utils'
+export * from './Types'
+export * from './Store'
+export * from './Defaults'
+export * from './WABinary'
+export * from './WAM'
+
+export type WASocket = ReturnType<typeof makeWASocket>
+export { makeWASocket, proto }
+export default makeWASocket
